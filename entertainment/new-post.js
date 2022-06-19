2711082c2f75777e5813d403ff0c3b04500f3c0c
9jahotblog <100433370+9jahotblog@users.noreply.github.com>
2022-06-19 15:21:05 +0100

    Create new-post.js

---
+++ entertainment/new-post.js
@@ -0,0 +1,262 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+<img src="https://i.postimg.cc/s2CXMdWT/9jahotblog-LOGO.jpg" width="250" height="250">
+       <p style="color:#0000FF">WELCOME TO 9JAHOTBLOG</a>
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="https://9jahotblog.github.io">Home</a>
+  <a href="https://9jahotblog.github.io/entertainment/will.html">Entertainment, Facts & Stories</a>
+  <a href="https://9jahotblog.github.io/Articles/change-and-correction-of-names-by-mere-affidavit-is-no-longer-valid.html">Articles & News</a>
+  <a href="https://9jahotblog.github.io/discussions/how-to-create-whatsapp-robot.html">Forum, Discussions</a>
+  <a href="https://9jahotblog.github.io/Education/citizenship-topic.html">Education</a>
+  <a href="https://9jahotblog.github.io/documentary/animal-mixup.html">Documentary</p>
+  <a href="https://9jahotblog.github.io/aboutus.html">About Us</a>
+  <a href="https://9jahotblog.github.io/contact-us.html">Contact Us</a>
+  <a href="https://9jahotblog.github.io/pp.html">Privacy Policy</a>
+</div>
+
+
+ 
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+   </body>
+</html> 
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>IMPACT MAKING (My opinion)</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+.w3-bar-block .w3-bar-item {padding:20px}
+</style>
+</head>
+<body>
+
+
+ <div class="w3-content" style="max-width:1400px">
+
+ 
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>9JAHOT BLOG</b></h1>
+  <p>Welcome to the <span class="w3-tag">Hottest Blog🇳🇬</span></p>
+</header>
+
+
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center" id="food">
+    <div class="w3-quarter">
+    <img src="https://i.postimg.cc/j2XjkxqX/9jahotblogimpactmaking-LOGO.jpg" width="250" height="250">
+    <p style="color:#0000FF">IMPACT MAKING</p>
+    <p style="color:#0000FF">By Genamie</p>
+   
+<p style="color:#0000FF">Today, someone randomly asked me a question and I quote “what does failure mean to me?”</p> It came unexpectedly, I couldn’t fast track my brain to come up with a reasonable answer so I said “I don’t know” and then he said and I quote “to me it means living all I have to live in this world and die without making an IMPACT.” when he said that the question that got stuck in my mind is “what is impact?”, “what does making impact, mean?”
+<p style="color:#0000FF">If I’m a doctor and I save lives every day, I’m I making enough IMPACT.</p>
+<p style="color:#0000FF">If I’m a firefighter and I save lives every day, I’m I making enough IMPACT.</p>
+<p style="color:#0000FF">If I’m a teacher grooming and teaching the future of the world, am I making IMPACT at all?</p>
+<p style="color:#0000FF">If I’m a mother and I’m bring up the leaders of tomorrow, can that be considered as IMPACT?</p>
+<p style="color:#0000FF">This analysis basically takes us back to “what is IMPACT?”</p> Dictionary says “to have an important or noticeable effect on someone or something.” The dictionary also defines “MAKER” as “someone who does something or makes something happen.” So that means an IMPACT MAKER in this world is someone who has an important and noticeable effect in this world. "Noticeable" is where my problem with this definition is at. None ever noticed the impact a good mother makes in bringing up good children that become good adults that eventually become responsible human beings. Also no one ever notices the impact of a good teacher that grooms and teaches students not just academically as they are not paid enough to do, but morally because he or she wants the students to have a better life which will in turn make those student responsible.
+<p style="color:#0000FF">Neither is it often for someone to consciously or noticeably appreciate a nurse.</p>Have you ever been to a hospital or watched a medically oriented movie say good doctor, you would notice that it’s the nurses that gives the drugs, show care, enquire about what they need to make them more comfortable. To be honest I would be undoubtedly grateful to the doctor that fixed my spine but I’m sure to also thank the nurse that carried me to the toilet when I couldn’t walk. At this juncture, I hope I have not confused you but convinced you that an impact doesn’t have to be noticed by the world to be an IMPACT…. It just has to have a positive effect on the world to be an IMPACT.
+<p style="color:#0000FF">So give a standing ovation to the doctors,</p> clap your hands for those flying to the moon and inventing electricity, just remember to constantly say THANKYOU to your parents, teachers, nurses or any other person making a positively impacting this world.
+<p style="color:#0000FF">Are you or anyone you know making an impact in the world, and you think it’s underappreciated….</p>
+<p style="color:#0000FF">Tell me about it, let’s get familiar in the comments section.</p>
+
+
+
+</div>
+
+<a href="https://chat.whatsapp.com/CY1ekNLBsue1K7yDNv0RWb"> CLICK HERE TO JOIN OUR WHATSAPP GROUP</a>
+
+</div>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>9JAHOTBLOG</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>                     
+<style>
+h1,h2,h3,h4,h5,h6 {font-family: "Oswald"}
+body {font-family: "Open Sans"}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Navigation bar with social media icons -->
+<div class="w3-bar w3-black w3-hide-small">
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-facebook-official"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-instagram"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-snapchat"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-flickr"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-twitter"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-linkedin"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-whatsapp"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-envelope" style="font-size:48px;color:red"></i></a>
+  <a href="#" class="w3-bar-item w3-button"><i class="fa fa-telegram"></i></a>
+<br>
+
+  <a href="#" class="w3-bar-item w3-button w3-right"><i class="fa fa-search"></i></a>
+</div>
+  
+<div class="w3-white w3-margin">
+        <div class="w3-container w3-padding w3-black">
+          <h4>Share This Post</h4>
+        </div>
+        <div class="w3-container w3-xlarge w3-padding">
+          <a href="https://www.facebook.com/sharer/sharer.php?u=https://9jahotblog.github.io/entertainment/impact-making.html"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+          <a href="https://www.instagram.com/the_real_philip617"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+          <a href="https://msng.link/o/?kolade_ogun2019=sc"><i class="fa fa-snapchat w3-hover-opacity"></i></a>
+          <a href="https://pinterest.com/pin/create/button/?url=https://9jahotblog.github.io/entertainment/impact-making.html&media=&description=Impact%20Making%20Story%20Read%20it%20here!"><i class="fa fa-pinterest-p w3-hover-opacity"></i></a>
+          <a href="https://twitter.com/intent/tweet?url=https://9jahotblog.github.io/entertainment/impact-making.html&text=Impact%20Making%20Story%20Read%20it%20here!"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+          <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://9jahotblog.github.io/entertainment/impact-making.html"<i class="fa fa-linkedin w3-hover-opacity"></i></a>
+          <a href="https://api.whatsapp.com/send?text=*_Impact%20Making%20Story_*%20Read%20it%20here%20%F0%9F%94%A5%F0%9F%94%A5%0Ahttps%3A%2F%2F9jahotblog.github.io%2Fentertainment%2Fimpact-making.html"><i class="fa fa-whatsapp w3-hover-opacity"></i></a>
+          <a href="mailto:info@example.com?&subject=&cc=&bcc=&body=https://9jahotblog.github.io/entertainment/impact-making.html%0AImpact%20Making%20Story%20Read%20it%20here!"><i class="fa fa-envelope" w3-hover-opacity"></i></a>
+          <a href="https://xn--r1a.link/share/url?url=https%3A%2F%2F9jahotblog.github.io%2Fentertainment%2Fimpact-making.html&text=Impact%20Making%20Story%2C%20Read%20here%20%F0%9F%94%A5%F0%9F%94%A5"><i class="fa fa-telegram w3-hover-opacity"></i></a>
+        </div>
+      </div>
+      <hr>
+     
+<div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Read Also (Click Image)</h4>
+    </div>
+    <ul class="w3-ul w3-hoverable w3-white">
+    <li class="w3-padding-16">
+        <a href="https://9jahotblog.github.io/entertainment/the-indecisiveness-of-a-teenager.html"><img src="https://i.postimg.cc/sXRpqzXk/indecisiveness.jpg" alt="Image" class="w3-left w3-margin-right" style="width:100px"></a>
+   <span class="w3-large">The Indecisiveness of a Teenager</span><br>
+        <span>(MY STORY) I’m my age and I’m about to enter the university and 3 months ago I had no Idea what I wanted to study and I basically had so many things I was passionate about that I had no passion at all....</span>
+      </li>   
+     <li class="w3-padding-16">
+     <a href="https://9jahotblog.github.io/Education/olympus-12-greek-gods.html"><img src="https://i.postimg.cc/7PKCgTZ3/poseidonstatue.jpg" alt="9jahotblog" class="w3-left w3-margin-right" style="width:100px"></a>
+        <span class="w3-large">Meet the Greek Gods </span><br>
+        <span>Poseidon was a very greedy, moody & bad-tempered god. He was a brother to Zeus & Hades. Son of Cronus & Rhea. 
+    He is the god of the sea & earthquakes. He was the creator of the storms,  Poseidon helped Zeus overthrow is father.......
+ </span>
+       </li>
+      <li class="w3-padding-16">
+        <a href="https://9jahotblog.github.io/entertainment/the-uncertainty-of-life.html"><img src="https://i.postimg.cc/N02w2fJ3/Text-Pro-me-1629a30b804df5-2.jpg" alt="Image" class="w3-left w3-margin-right" style="width:100px"></a>
+   <span class="w3-large">THE UNCERTAINTY OF LIFE2</span><br>
+        <span>This days I have been focusing all my THINKING energy on the future and what’s to come.
+And I can’t help but wonder what if things don’t go the way I and others have planned be it good or bad......</span>
+      </li>   
+      <li class="w3-padding-16">
+      <a href="https://9jahotblog.github.io/entertainment/will.html"><img src="https://i.postimg.cc/d1kdrLkx/FB-IMG-1648751901570.jpg" alt="Will Smith" class="w3-left w3-margin-right" style="width:100px"></a>
+   <span class="w3-large">Will Smith gets Banned 10 for years </span><br>
+        <span> American Actor Will Smith gets banned from the Oscars for 10 years for hitting Chris Rock......</span>
+      </li>
+       <li class="w3-padding-16">
+        <a href="https://9jahotblog.github.io/discussions/how-to-create-whatsapp-robot.html"><img src="https://i.postimg.cc/rFpQ7bMM/Ephoto360-com-16219090b89a51.jpg" alt="9jahotblog" class="w3-left w3-margin-right" style="width:100px"></a>
+        <span class="w3-large">How to Create a WhatsApp robot? Full tutorial. </span><br>
+        <span>Things you don't know about WhatsApp robot, It can download & upload songs within 2 secs.  It can download & upload videos from Instagram, YouTube e.t.c within 3 secs..- It can chat with your......</span>
+        </li>
+    </ul>
+  </div>
+  <hr> 
+
+
+
+<script src="https://cdn.fastcomments.com/js/embed.min.js"></script>
+<div id="fastcomments-widget"></div>
+<script>
+    FastCommentsUI(document.getElementById('fastcomments-widget'), {
+        tenantId: 'AC6VPRnrDyH'
+    });
+</script>
+
+
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-38V179JCWD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-38V179JCWD');
+</script>
+
+
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+  <button class="w3-button w3-black w3-padding-large w3-margin-bottom"><a href="https://9jahotblog.github.io/entertainment/the-uncertainty-of-life.html">Previous »</button></a>
+  <button class="w3-button w3-black w3-padding-large w3-margin-bottom"><a href="https://9jahotblog.github.io/discussions/what-is-affiliate-marketing.html">Next »</button></a>
+  <p>Powered by <a href="https://Instagram.com/the_real_philip617" target="_blank">Philip Smith</a></p>
+</footer>
+</body>
+</html>
+</div>
